6a948eb3fcf3f688ec614944330f67e6a680cae3
Jesus Hincapie <jahlon@gmail.com>
2022-02-16 09:55:38 -0500

    Implementacion de ejemplos del tutorial

---
+++ TutorialPOO/EjemploDiccionario.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TutorialPOO
+{
+    class EjemploDiccionario
+    {
+        static void Main(string[] args)
+        {
+            IDictionary<int, string> numberNames = new Dictionary<int, string>();
+            numberNames.Add(1, "One"); //adding a key/value using the Add() method
+            numberNames.Add(2, "Two");
+            numberNames.Add(3, "Three");
+
+            foreach(KeyValuePair<int, string> kvp in numberNames)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+            }
+
+            var cities = new Dictionary<string, string>(){
+                {"UK", "London, Manchester, Birmingham"},
+                {"USA", "Chicago, New York, Washington"},
+                {"India", "Mumbai, New Delhi, Pune"}
+            };
+
+            foreach (var kvp in cities)
+                Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+
+            Console.WriteLine(cities["UK"]); //prints value of UK key
+            Console.WriteLine(cities["USA"]);//prints value of USA key
+
+            if( cities.ContainsKey("France"))
+                Console.WriteLine(cities["France"]);
+
+            string result;
+
+            if (cities.TryGetValue("France", out result))
+            {
+                Console.WriteLine(result);
+            }
+
+            cities["UK"] = "Nuevo valor";
+            cities.Remove("USA");
+
+
+            Console.ReadLine();
+        }
+        
+    }
+}
--- TutorialPOO/Program.cs
+++ TutorialPOO/Program.cs
@@ -8,8 +8,43 @@ namespace TutorialPOO
 {
     class Program
     {
+        /*
         static void Main(string[] args)
         {
+            var account = new BankAccount("Jesus", 1000);
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine($"Balance {account.Balance}");
+            account.MakeDeposit(100, DateTime.Now, "Friend pay me back");
+            Console.WriteLine($"Balance {account.Balance}");
+
+            BankAccount invalidAccount;
+            try
+            {
+                invalidAccount = new BankAccount("invalid", 55);
+            }
+            catch(ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+                Console.ReadLine();
+            }
+
+            try
+            {
+                account.MakeWithdrawal(400, DateTime.Now, "Attempt to withdrawal");
+            }
+            catch(InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+                Console.ReadLine();
+            }
+
+            Console.WriteLine(account.GetAccountHistory());
+            Console.ReadLine();
+
         }
+        */
+        
     }
 }
